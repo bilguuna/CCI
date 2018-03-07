e35e95034187ae09bb147357ea745ac9d2f78367
bilguuna <ariunboldbiliguun@gmail.com>
2018-03-07 14:23:47 -0500

    Q1_6 Added String builder version

--- solutions/src/bilguun/ariunbold/solutions/chapter1/Q1_6.java
+++ solutions/src/bilguun/ariunbold/solutions/chapter1/Q1_6.java
@@ -7,6 +7,7 @@ package bilguun.ariunbold.solutions.chapter1;
 public class Q1_6 {
     public static void main(String[] args) {
         System.out.println(compress("a"));
+        System.out.println(compressWithStringBuilder("aabbbbbccccccxx"));
     }
 
     static String compress(String s) {
@@ -37,4 +38,31 @@ public class Q1_6 {
             return r;
         }
     }
+
+    static String compressWithStringBuilder(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        StringBuilder r = new StringBuilder();
+        int num = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            num++;
+
+            if (i + 1 >= s.length() || s.charAt(i) != s.charAt(i + 1)) {
+                r.append(s.charAt(i));
+                r.append(num);
+                num = 0;
+            }
+        }
+
+        if (s.length() < r.length()) {
+            return s;
+        } else {
+            return r.toString();
+        }
+    }
 }
+
+
