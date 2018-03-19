781e4dacb4e77c1e5181410262d2a3ec9b5c4121
bilguuna <ariunboldbiliguun@gmail.com>
2018-03-19 16:17:58 -0400

    Q1_8 Added

---
+++ solutions/src/bilguun/ariunbold/solutions/chapter1/Q1_8.java
@@ -0,0 +1,78 @@
+package bilguun.ariunbold.solutions.chapter1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author bilguuna
+ */
+public class Q1_8 {
+    public static void main(String[] args) {
+        Integer n = 5;
+        Integer m = 6;
+        Integer[][] arr = initArray(n, m);
+        arr[0][0] = 0;
+        arr[1][2] = 0;
+        arr[2][3] = 0;
+        print(arr, n, m);
+
+        System.out.println("\n\n---------AFTER ZEROED THE ARRAY-----------");
+        zeroMatrix(arr, n, m);
+        print(arr, n, m);
+
+
+    }
+
+    static Integer[][] zeroMatrix(Integer[][] arr, int n, int m) {
+        Set<Integer> zeroedCols = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (zeroedCols.contains(j)) {
+                    continue;
+                }
+
+                if (arr[i][j] == 0) {
+                    for (int k = 0; k < n; k++) {
+                        arr[k][j] = 0;
+                    }
+
+                    for (int k = 0; k < m; k++) {
+                        arr[i][k] = 0;
+                    }
+
+                    zeroedCols.add(j);
+                    break;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    static void print(Integer[][] arr, Integer n, Integer m) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("");
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] > 9) {
+                    System.out.print(arr[i][j] + "  ");
+                } else {
+                    System.out.print(arr[i][j] + "   ");
+                }
+            }
+        }
+    }
+
+    static Integer[][] initArray(int n, int m) {
+        Integer[][] r = new Integer[n][m];
+        Integer curr = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                r[i][j] = curr++;
+            }
+        }
+
+        return r;
+    }
+}
