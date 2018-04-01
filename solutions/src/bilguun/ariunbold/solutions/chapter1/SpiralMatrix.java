f1a6c3d06c74eef8f1eab089eebdc8802e3ce217
bilguuna <bil.ariunbold@gmail.com>
2018-04-01 11:49:29 -0400

    SpiralMatrix

---
+++ solutions/src/bilguun/ariunbold/solutions/chapter1/SpiralMatrix.java
@@ -0,0 +1,64 @@
+package bilguun.ariunbold.solutions.chapter1;
+
+/**
+ * @author bilguuna
+ */
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Integer n = 6;
+        Integer[][] arr = initArray(n);
+        print(arr, n);
+
+        System.out.println("PRINT SPIRALLY:");
+        spiralPrint(arr, n);
+    }
+
+    static void spiralPrint(Integer[][] arr, int n) {
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i; j++) {
+                System.out.print(arr[i][j] + "  ");
+            }
+
+            for (int j = i + 1; j < n - i; j++) {
+                System.out.print(arr[j][n - 1 - i] + "  ");
+            }
+
+            for (int j = i; j < n - 1 - i; j++) {
+                System.out.print(arr[n - 1 - i][n - 2 - j] + "  ");
+            }
+
+            for (int j = n - 2 - i; j > i; j--) {
+                System.out.print(arr[j][i] + "  ");
+            }
+
+        }
+    }
+
+    static Integer[][] initArray(int n) {
+        Integer[][] r = new Integer[n][n];
+        Integer curr = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                r[i][j] = curr++;
+            }
+        }
+
+        return r;
+    }
+
+    static void print(Integer[][] arr, Integer n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("");
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] > 9) {
+                    System.out.print(arr[i][j] + "  ");
+                } else {
+                    System.out.print(arr[i][j] + "   ");
+                }
+            }
+        }
+    }
+}
+
+
