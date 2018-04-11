b46a25604e2558bd70537a8e0538d919fadcbfcf
bilguuna <ariunboldbiliguun@gmail.com>
2018-04-11 12:48:28 -0400

    Modified Q2 2

--- solutions/src/bilguun/ariunbold/solutions/chapter2/Q2_2.java
+++ solutions/src/bilguun/ariunbold/solutions/chapter2/Q2_2.java
@@ -8,33 +8,71 @@ public class Q2_2 {
     public static void main(String[] args) {
         Node<Integer> head = Utils.initLinkedList(20);
         Utils.print(head);
-        int k = 6;
-        moveToLast(head, k);
-        System.out.println("AFTER MOVING " + k + "th ELEMENT: ");
-        Utils.print(head);
+        int k = 4;
+        System.out.println(k + "th to the last is : " + getKth(head, k).getValue());
+        System.out.println(k + "th to the last is(2 pointers) : " + getKthByTwoPointers(head, k).getValue());
+        getKthRecursively(head, k);
+
     }
 
-    static void moveToLast(Node<Integer> node, int k) {
-        int i = 1;
-        Node<Integer> currNode = node;
-        Node kthNode = null;
-
-        if(k == 1) {
-            kthNode = node;
+    static int getKthRecursively(Node node, int k) {
+        if (node == null) {
+            return 0;
         }
 
-        while(currNode.getNext() != null) {
-            i++;
+        int index = getKthRecursively(node.getNext(), k) + 1;
+        if (index == k) {
+            System.out.println(k + "th to the last is(Recursively) : " + node.getValue());
+        }
 
-            if(i == k) {
-                kthNode = currNode.getNext();
-                currNode.setNext(currNode.getNext().getNext());
-                kthNode.setNext(null);
+        return index;
+    }
+
+
+    static Node getKth(Node node, int k) {
+        Node curr = node;
+        int length = 0;
+
+        while (curr != null) {
+            curr = curr.getNext();
+            length++;
+        }
+
+        if (k > length) {
+            return null;
+        }
+
+        curr = node;
+        int i = 0;
+
+        while (curr != null && i < (length - k)) {
+            if (i == (length - k)) {
+                return curr;
+            }
+            curr = curr.getNext();
+            i++;
+        }
+
+        return curr;
+    }
+
+    static Node getKthByTwoPointers(Node node, int k) {
+        Node p1 = node;
+        Node p2 = node;
+
+        for (int i = 0; i < k; i++) {
+            if (p1 == null) {
+                return null;
             }
 
-            currNode = currNode.getNext();
+            p1 = p1.getNext();
         }
 
-        currNode.setNext(kthNode);
+        while (p1 != null) {
+            p1 = p1.getNext();
+            p2 = p2.getNext();
+        }
+
+        return p2;
     }
 }
